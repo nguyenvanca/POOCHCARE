ff0ab38047178eb257be18924af746084892cb32
mielindev <phamvietquang.dev@gmail.com>
2024-08-01 16:00:12 +0700

    Update style.js

--- js/style.js
+++ js/style.js
@@ -15,3 +15,20 @@ document.getElementById("switchButton").onclick = function () {
   //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
   document.getElementById("myBody").classList.toggle("dark");
 };
+
+// Button Move On Top
+let myButton = document.querySelector("#myBtn");
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
